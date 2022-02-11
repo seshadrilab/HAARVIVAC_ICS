823aa85fff6ae2e956396f74943ee83a12054bd7
Jolie Phan <jolie.m.phan@gmail.com>
2022-02-11 09:28:47 -0800

    Add boolean gates manually

--- scripts/04_Add_boolean_gates_attempt3.R
+++ scripts/04_Add_boolean_gates_attempt3.R
@@ -8,6 +8,7 @@ gsPath <- here::here("out/GatingSets/HAARVIVAC_GatingSet")
 gs <- load_gs(gsPath)
 
 ## Add CD4 boolean gates ##
+# Look at the nodes in the gating hierarchy
 gh_get_pop_paths(gs) # 31 nodes total
 
 # List of partial gating paths (these are existing gates)
@@ -27,13 +28,14 @@ bool_list_polyf <- bool_list[!single]
 # Concatenate the boolean subsets because they're a list of lists right now
 bool_list_polyf_cat <- stri_join_list(bool_list_polyf, sep = "&", collapse = NULL)
 
-# Reformat the boolean subsets in string format (e.g., CD4+/IL2+&CD4+/IFNg+&!CD4+/TNFa+)
+# Reformat the subsets in proper boolean logic (e.g., CD4+/IL2+&CD4+/IFNg+&!CD4+/TNFa+)
 bool_list_polyf_cat_long <- vector("list", 120)
 
 for(i in seq_along(bool_list_polyf_cat)){
   out <- bool_list_polyf_cat[i]
   for(k in seq_along(cd4_paths)){
     new_subset <- out
+    # Add logical negation (!) for every marker not part of the subset
     if(isFALSE(grepl(cd4_paths[k], new_subset, fixed = TRUE))){
       out <- paste(new_subset, cd4_paths[k], sep = "&!")
     }
@@ -41,7 +43,9 @@ for(i in seq_along(bool_list_polyf_cat)){
   bool_list_polyf_cat_long[i] <- out
 }
 
-# Assign shorter names to each boolean subset in the list
+bool_list_polyf_cat_long <- unlist(bool_list_polyf_cat_long)
+
+# Assign shortened names to each boolean subset in the list
 names(bool_list_polyf_cat_long) <- bool_list_polyf_cat
 
 # Add individual gates
@@ -58,39 +62,9 @@ gs_pop_add(gs, eval(substitute(flowWorkspace::booleanFilter(v),
 # Check gating tree
 gh_get_pop_paths(gs) # 152 nodes total
 
-# Recompute GatingSet 
-recompute(gs, "/Time/CD3+/CD14-CD19-/Lymphocytes/Singlet/Live/CD4+")
-
-# Compare counts between the sum of the individual boolean gates vs CD4_PolyF_Subsets
-# Counts from the individual boolean subset gates (excluding SEB)
-indiv_pop_counts <- pData(gs) %>%
-  tibble::rownames_to_column("rowname") %>%
-  left_join(gs_pop_get_count_fast(gs, subpopulations = gs_get_pop_paths(gs)[32:151]), by = c("rowname" = "name")) %>%
-  dplyr::rename(CD4_PolyF = Count) %>%
-  dplyr::select(rowname, "SAMPLE ID", "EXPERIMENT NAME", Stim, Group, CD4_PolyF) %>%
-  dplyr::filter(Stim != "SEB")
-
-sum(pop_counts$CD4_PolyF) # 1402
-
-# Counts from CD4_PolyF_Subsets gate (excluding SEB)
+## Add activation gate (HLADR+CD38+) under CD4_PolyF_Subsets ##
 cd4_polyf_parent <- "/Time/CD3+/CD14-CD19-/Lymphocytes/Singlet/Live/CD4+/CD4_PolyF_Subsets"
 
-pop_counts <- pData(gs) %>%
-  tibble::rownames_to_column("rowname") %>%
-  left_join(gs_pop_get_count_fast(gs, subpopulations = cd4_polyf_parent), by = c("rowname" = "name")) %>%
-  dplyr::rename(CD4_PolyF = Count) %>%
-  dplyr::select(rowname, "SAMPLE ID", "EXPERIMENT NAME", Stim, Group, CD4_PolyF) %>%
-  dplyr::filter(Stim != "SEB")
-
-sum(pop_counts$CD4_PolyF) # 1402
-
-## Save GatingSet ##
-#save_gs(gs, here::here("out/GatingSets/HAARVIVAC_GatingSet_with_bool3"))
-#gs <- load_gs(here::here("out/GatingSets/HAARVIVAC_GatingSet_with_bool3"))
-
-#############################################################
-
-## Add activation gate (HLADR+CD38+) under CD4_PolyF_Subsets ##
 hladr_path <- "/Time/CD3+/CD14-CD19-/Lymphocytes/Singlet/Live/HLADR+"
 cd38_path <- "/Time/CD3+/CD14-CD19-/Lymphocytes/Singlet/Live/CD38+"
 
@@ -99,7 +73,7 @@ gs_pop_add(gs, eval(substitute(flowWorkspace::booleanFilter(v),
                                                          "&", cd38_path))))),
            parent = cd4_polyf_parent, name = "HLADR+CD38+")
 
-## Add memory gates under boolean gates ##
+## Add memory gates under CD4_PolyF_Subsets ##
 cd4_ccr7_path <- "/Time/CD3+/CD14-CD19-/Lymphocytes/Singlet/Live/CD4+/CCR7+"
 cd4_cd45ra_path <- "/Time/CD3+/CD14-CD19-/Lymphocytes/Singlet/Live/CD4+/CD45RA+"
 
@@ -123,13 +97,37 @@ gs_pop_add(gs, eval(substitute(flowWorkspace::booleanFilter(v),
                                                          "&!", cd4_cd45ra_path))))),
            parent = cd4_polyf_parent, name = "TEM")
 
-# Recompute GatingSet
-recompute(gs, "/Time/CD3+/CD14-CD19-/Lymphocytes/Singlet/Live/CD4+")
+## Recompute GatingSet ##
+flowWorkspace::recompute(gs, "/Time/CD3+/CD14-CD19-/Lymphocytes/Singlet/Live/CD4+")
 
-# Plot gating tree
-png(here::here("out/GatingTree_with_Bool_Subsets.png"), width = 7, height = 5, units = "in", res = 300)
-plot(gs, bool = T, fontsize = 10)
-dev.off()
+## Compare counts between the sum of the individual boolean gates vs CD4_PolyF_Subsets ##
+# Counts from the individual boolean subset gates (excluding SEB)
+indiv_pop_counts <- pData(gs) %>%
+  tibble::rownames_to_column("rowname") %>%
+  left_join(gs_pop_get_count_fast(gs, subpopulations = gs_get_pop_paths(gs)[32:151]), by = c("rowname" = "name")) %>%
+  dplyr::rename(CD4_PolyF = Count) %>%
+  dplyr::select(rowname, "SAMPLE ID", "EXPERIMENT NAME", Stim, Group, CD4_PolyF) %>%
+  dplyr::filter(Stim != "SEB")
 
-# Save GatingSet 
-save_gs(gs, here::here("out/GatingSets/HAARVIVAC_GatingSet_with_bool3"))
+sum(indiv_pop_counts$CD4_PolyF) # 341185
+
+# Counts from CD4_PolyF_Subsets gate (excluding SEB)
+pop_counts <- pData(gs) %>%
+  tibble::rownames_to_column("rowname") %>%
+  left_join(gs_pop_get_count_fast(gs, subpopulations = cd4_polyf_parent), by = c("rowname" = "name")) %>%
+  dplyr::rename(CD4_PolyF = Count) %>%
+  dplyr::select(rowname, "SAMPLE ID", "EXPERIMENT NAME", Stim, Group, CD4_PolyF) %>%
+  dplyr::filter(Stim != "SEB")
+
+sum(pop_counts$CD4_PolyF) # 1444
+
+# Something is wrong, the counts are really different and they keep changing
+
+# ## Plot gating tree ##
+# png(here::here("out/GatingTree_with_Bool_Subsets.png"), width = 7, height = 5, units = "in", res = 300)
+# plot(gs, bool = T, fontsize = 10)
+# dev.off()
+# 
+# ## Save GatingSet ##
+# save_gs(gs, here::here("out/GatingSets/HAARVIVAC_GatingSet_with_bool"))
+#gs <- load_gs(here::here("out/GatingSets/HAARVIVAC_GatingSet_with_bool"))
