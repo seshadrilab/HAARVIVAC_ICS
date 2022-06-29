a3fc3b107450fb78610f4767b70c0fdaac117ba9
Jolie Phan <jolie.m.phan@gmail.com>
2022-06-28 18:34:05 -0700

    Add system.time

--- scripts/02_Run_COMPASS.R
+++ scripts/02_Run_COMPASS.R
@@ -19,7 +19,6 @@ library(furrr)
 library(COMPASS)
 library(grid)
 library(flowWorkspace)
-library(tictoc)
 
 source(here::here("scripts/Helper_Functions.R"))
 
@@ -49,43 +48,43 @@ future::supportsMulticore() # Run in terminal to get TRUE
 # see ?future::supportsMulticore "
 future::plan(multicore(workers = max(1, availableCores() - 2)))
 
-tic()
-out <- furrr::future_pmap(.l = list(stims_for_compass_runs_rep,
-                                    parent_nodes_for_compass_runs_rep,
-                                    seeds_for_compass_runs),
-                          .f = function(currentStim, parent, currentSeed) {
-                            
-                            o <- tryCatch( {
-                              gsSub <- subset(gs, Stim %in% c("DMSO", currentStim))
+system.time({
+  out <- furrr::future_pmap(.l = list(stims_for_compass_runs_rep,
+                                      parent_nodes_for_compass_runs_rep,
+                                      seeds_for_compass_runs),
+                            .f = function(currentStim, parent, currentSeed) {
                               
-                              
-                              
-                              currentNodeMarkerMap <- mapMarkers
-                              # currentNodeMarkerMap names are gating tree paths
-                              names(currentNodeMarkerMap) <- paste0(parent, "/", c("IL2+", "IL4_5_13+", "IFNg+", "TNFa+", "IL17a+", "CD154+", "CD107a+"))
-                              outDir <- here::here(sprintf("out/CompassOutput/%s/%s", parent, gsub(" ", "_", currentStim)))
-                              if(!dir.exists(outDir)) {
-                                dir.create(outDir, recursive = T)
-                              }
-                              
-                              runCompassOnce(gs=gsSub,
-                                             seed=currentSeed,
-                                             outDir=outDir,
-                                             parentNode=parent,
-                                             nodeMarkerMap=currentNodeMarkerMap,
-                                             uniqueIdentifier="SAMPLE ID",
-                                             treatmentCol="Stim",
-                                             currentTreatment=currentStim,
-                                             currentControl="DMSO",
-                                             stratifyBy=NULL, 
-                                             iter=40000,
-                                             eventCountFilterThreshold=3000,
-                                             textForRunOutputId=paste0(parent, "_", gsub(" ", "_", currentStim)))
-                              gc()
-                            }, error = function(e) { print(e) })
-                            o
-                          },
-                          # Progress bar reflects how many COMPASS runs have completed
-                          .progress = T,
-                          .options = furrr_options(seed = T))
-toc()
+                              o <- tryCatch( {
+                                gsSub <- subset(gs, Stim %in% c("DMSO", currentStim))
+                                
+                                
+                                
+                                currentNodeMarkerMap <- mapMarkers
+                                # currentNodeMarkerMap names are gating tree paths
+                                names(currentNodeMarkerMap) <- paste0(parent, "/", c("IL2+", "IL4_5_13+", "IFNg+", "TNFa+", "IL17a+", "CD154+", "CD107a+"))
+                                outDir <- here::here(sprintf("out/CompassOutput/%s/%s", parent, gsub(" ", "_", currentStim)))
+                                if(!dir.exists(outDir)) {
+                                  dir.create(outDir, recursive = T)
+                                }
+                                
+                                runCompassOnce(gs=gsSub,
+                                               seed=currentSeed,
+                                               outDir=outDir,
+                                               parentNode=parent,
+                                               nodeMarkerMap=currentNodeMarkerMap,
+                                               uniqueIdentifier="SAMPLE ID",
+                                               treatmentCol="Stim",
+                                               currentTreatment=currentStim,
+                                               currentControl="DMSO",
+                                               stratifyBy=NULL, 
+                                               iter=40000,
+                                               eventCountFilterThreshold=3000,
+                                               textForRunOutputId=paste0(parent, "_", gsub(" ", "_", currentStim)))
+                                gc()
+                              }, error = function(e) { print(e) })
+                              o
+                            },
+                            # Progress bar reflects how many COMPASS runs have completed
+                            .progress = T,
+                            .options = furrr_options(seed = T))
+})
